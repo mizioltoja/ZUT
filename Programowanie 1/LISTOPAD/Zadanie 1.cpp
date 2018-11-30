d649a0dd41cdf9339975305d1ae0872052b66648
mizioltoja@gmail.com <mizioltoja@gmail.com>
2018-11-30 21:08:02 +0100

    Zadanie 1.1

--- Programowanie 1/LISTOPAD/Zadanie 1.cpp
+++ Programowanie 1/LISTOPAD/Zadanie 1.cpp
@@ -1,1 +1,21 @@
-huehue
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void zad11()
+{
+	int a=5;
+	float b=5.555;
+	char c=33;
+	
+	cout<<"Zmienna int ="<<a<<endl;
+	cout<<"Zmienna float ="<<b<<endl;
+	cout<<"Zmienna char ="<<c<<endl;	
+}
+
+int main ()
+{
+	zad11();
+	return 0;
+}
